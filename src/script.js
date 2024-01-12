2ad3d9df48bf5534080e7154b0a88f422a626442
noname1112392 <149623681+noname1112392@users.noreply.github.com>
2024-01-12 09:51:31 -0800

    Create a dialog on html/ Create an addeventlistener for the dialog add button

---
+++ src/script.js
@@ -0,0 +1,8 @@
+// Global Variables
+const addBtnDialog = document.getElementById('add-btn-dialog');
+const dialog = document.getElementById('dialog');
+
+//Function to show dialog
+addBtnDialog.addEventListener('click', () => {
+    dialog.showModal();
+})
