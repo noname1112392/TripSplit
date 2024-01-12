0bd4caa3bc9e8e7c6d331bd19df8073cc606d3bb
noname1112392 <149623681+noname1112392@users.noreply.github.com>
2024-01-12 10:45:27 -0800

    Create an entityStorage array / Create constructor of the entity

--- src/script.js
+++ src/script.js
@@ -7,6 +7,15 @@ const submitBtn = document.getElementById('submit-btn');
 const closeBtn = document.getElementById('close-btn');
 const outputContainer = document.getElementById('output-container');
 
+//An array to store entity's objects
+const entityStorage = [];
+
+//Constructor of the entity
+function Entity(name, money) {
+    this.name = name;
+    this.money = money;
+}
+
 //Function to show dialog
 addBtnDialog.addEventListener('click', () => {
     dialog.showModal();
@@ -17,3 +26,33 @@ closeBtn.addEventListener('click', (e) => {
     e.preventDefault();
     dialog.close();
 })
+
+//Output the layout structure
+function outputLayout() {
+    //Box container the output
+    const box = document.createElement('div');
+    box.className = 'flex justify-between items-center px-5 rounded-lg shadow-md p-2';
+
+    //Name container
+    const nameContainer = document.createElement('div');
+    //p tag inside name container
+    const pName = document.createElement('p');
+    //name container append p tag
+    nameContainer.appendChild(pName);
+
+    //create a divider between name and the money they spend/owe
+    const divider = document.createElement('div');
+    divider.className = 'font-bold text-2xl';
+
+    //Money container
+    const moneyContainer = document.createElement('div');
+    moneyContainer.className = 'flex space-x-2';
+    //two p tags inside the money container
+    //this p tag of pay or owe will change later
+    //will create an if else statement to determine
+    const pPayOwe = document.createElement('p');
+    const moneyDisplay = document.createElement('p');
+    //money container append 2 p tags
+    moneyContainer.appendChild(pPayOwe);
+    moneyContainer.appendChild(moneyDisplay);
+}
