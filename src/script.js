fa8acca85b890a2b76e9e2050533aead66023016
noname1112392 <149623681+noname1112392@users.noreply.github.com>
2024-01-12 10:28:28 -0800

    Add global variables/Created closebtn function in js

--- src/script.js
+++ src/script.js
@@ -4,9 +4,16 @@ const dialog = document.getElementById('dialog');
 const nameInput = document.getElementById('name-input');
 const moneyInput = document.getElementsByName('money-input');
 const submitBtn = document.getElementById('submit-btn');
-
+const closeBtn = document.getElementById('close-btn');
+const outputContainer = document.getElementById('output-container');
 
 //Function to show dialog
 addBtnDialog.addEventListener('click', () => {
     dialog.showModal();
+})
+
+//Function to close dialog
+closeBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    dialog.close();
 })
