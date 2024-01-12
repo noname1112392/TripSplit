9c23f82d9d4077c57563939c453858f83cdc7faf
noname1112392 <149623681+noname1112392@users.noreply.github.com>
2024-01-12 10:23:02 -0800

    Created the dialog layout

--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
 // Global Variables
 const addBtnDialog = document.getElementById('add-btn-dialog');
 const dialog = document.getElementById('dialog');
+const nameInput = document.getElementById('name-input');
+const moneyInput = document.getElementsByName('money-input');
+const submitBtn = document.getElementById('submit-btn');
+
 
 //Function to show dialog
 addBtnDialog.addEventListener('click', () => {
